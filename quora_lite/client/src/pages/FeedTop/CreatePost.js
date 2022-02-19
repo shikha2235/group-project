440406aeeb7527068eeba25935073a219aaf8b04
squid41 <73808569+squid41@users.noreply.github.com>
2022-02-19 16:19:48 +0530

    Added FeedTop

---
+++ quora_lite/client/src/pages/FeedTop/CreatePost.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import CloseIcon from '@mui/icons-material/Close';
+const Modal_Styles={
+    backgroundColor: 'azure',
+    position:'fixed',
+    width:'43%',
+     marginTop:'30%',
+    // marginLeft:'30%',
+    // marginRight:'30%',
+    // marginDown:'30%',
+    display:'flex',
+
+    zindex: 1000
+
+}
+const Overlay_Styles={
+    position:'fixed',
+    top:0,
+    left:0,
+    right:0,
+    bottom:0,
+    zindex:1000,
+    backgroundColor:'rgba(0,0,0,0.7)'
+}
+export default function CreatePost({open,children,onClose}) {
+    if(!open)
+    return null
+  return (
+      <>
+    <div style={Overlay_Styles}></div>
+    <div style={Modal_Styles}>
+    <div>
+        <button onClick={onClose}><CloseIcon/></button>
+        {children}
+        </div>
+    </div>
+    </>
+  )
+}
